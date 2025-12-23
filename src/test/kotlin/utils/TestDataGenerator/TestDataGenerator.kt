6fd7486f67010385fdbd30c18e26f308b57c6189
Bronito <BronitoNaturele@yandex.ru>
2025-12-23 16:58:29 +0700

    do structure 9

--- src/test/kotlin/utils/TestDataGenerator/TestDataGenerator.kt
+++ src/test/kotlin/utils/TestDataGenerator/TestDataGenerator.kt
@@ -3,9 +3,3 @@
 //Генерирует случайные/валидные данные для тестов (имена, email, ID и т. п.).
 //Позволяет избегать жёсткой привязки к конкретным значениям.
 
-package utils.TestDataGenerator
-
-object TestDataGenerator {
-    fun randomEmail(): String = "user${Random.nextInt(1000)}@example.com"
-    fun validUserRequest(): UserRequest = UserRequest("Test User", randomEmail())
-}
